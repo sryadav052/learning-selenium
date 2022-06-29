3b28edf687c5710b7b70cbf63f0dabdc4a9d3804
Gaurav Yadav <syadav3052@gmail.com>
2022-06-29 21:51:01 +0530

    star pattern

---
+++ Practice/src/Basic1/StarPattern.java
@@ -0,0 +1,19 @@
+package Basic1;
+
+public class StarPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j;
+		for(i=1;i<=5;i++)
+		{
+			for(j=1;j<=5;j++)
+			{
+				System.out.print("*");
+			}
+				System.out.print("\n");
+		}
+
+	}
+
+}
